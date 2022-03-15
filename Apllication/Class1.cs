1570bd57381ca46cd4719347730918ee4bc6add9
tulioresende <tulio.resende.ufmg@gmail.com>
2022-03-15 20:57:05 -0300

    add aplication project

---
+++ Apllication/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Apllication
+{
+    public class Class1
+    {
+        public string Test() => "test";
+    }
+}
--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Apllication;
 
 namespace WebApi.Controllers
 {
@@ -24,16 +21,11 @@ namespace WebApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            Class1 testClass = new Class1();
+            return testClass.Test();
+
         }
     }
 }
